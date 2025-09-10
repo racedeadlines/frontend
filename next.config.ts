e7ef797ab141db815d4f313f23076a8fb8addfbe
Michael Hwang <michaelinjeyhwang@gmail.com>
2025-09-10 16:21:29 -0400

    added mdx

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,21 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
+
+import createMDX from "@next/mdx"
 
 const nextConfig: NextConfig = {
+  pageExtensions: ["mdx", "tsx"],
   reactStrictMode: true,
   images: {
     domains: [], // Add any external image domains here if needed
   },
-};
+  experimental: {
+    mdxRs: true,
+  },
+}
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
