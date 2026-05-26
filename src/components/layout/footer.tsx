8dbe15640de086c951f0338dd6ecded1612d68dc
Andrew Li <andruwuli@gmail.com>
2026-05-26 17:26:03 -0400

    fix layout: 2-col max grid, footer aligned to content width

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -7,18 +7,20 @@ export default function Footer() {
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
 
   return (
-    <footer className="flex flex-col space-y-4 p-4 dark:bg-neutral-900">
-      <Link href="/" className="flex w-fit items-end space-x-4">
-        <Logo />
-        <span className="text-2xl font-semibold italic">MarathonTrack</span>
-      </Link>
-      <p className="flex items-center space-x-4 text-sm">
-        <strong className="uppercase">time zone</strong>
-        <span>{timeZone}</span>
-      </p>
-      <p className="flex items-center text-sm">
-        Copyright © {currentYear} MarathonTrack.com
-      </p>
+    <footer className="p-4 dark:bg-neutral-900">
+      <div className="mx-auto flex w-full max-w-4xl flex-col space-y-4">
+        <Link href="/" className="flex w-fit items-end space-x-4">
+          <Logo />
+          <span className="text-2xl font-semibold italic">MarathonTrack</span>
+        </Link>
+        <p className="flex items-center space-x-4 text-sm">
+          <strong className="uppercase">time zone</strong>
+          <span>{timeZone}</span>
+        </p>
+        <p className="flex items-center text-sm">
+          Copyright © {currentYear} MarathonTrack.com
+        </p>
+      </div>
     </footer>
   )
 }
